6a1f0de60187121e80f2cc3f9563662f04004f62
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2018-04-24 15:52:56 +0530

    Delete doub.py

--- doub.py
+++
@@ -1,5 +0,0 @@
-doubl=['needs industry','need industry','cut off','rank jee']
-doublr={
-    'needs industry':'fbfg','need industry':'fbfg','cut off':'right','rank jee':'uuuuuu'
-        }
-doubwords=['needs','need','industry','cut','off','rank','jee']
